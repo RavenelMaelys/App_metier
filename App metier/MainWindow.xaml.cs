6b6aacb080e23ed8ab3ecb724a852ba12ea92694
RavenelMaelys <maelys.ravenel@viacesi.fr>
2022-04-19 10:59:19 +0200

    Add project files.

---
+++ App metier/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace App_metier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Missions_btn_Click(object sender, RoutedEventArgs e)
+        {
+            /*Apréhender les projets
+             * Concevoir un plan de developpement
+             * le besoin du client
+             * Developpement
+             * Suivi du developememnt
+             * *evolution du besoin
+             * *gerer le developement (temps, imprévus)
+             * restitution du logiciel
+             * Support logiciel*/
+        }
+
+        private void Profil_btn_Click(object sender, RoutedEventArgs e)
+        {
+            /*Maitrise de logiciels (Environnement de developement)
+             * 
+             *
+             *
+             */
+        }
+    }
+}
